ee1fc74cd8d5a74924eec2298b25e6ef7facf186
SimonGideon <simongideon918@gmail.com>
2022-12-05 23:36:52 +0300

    Refactor code

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -58,16 +58,16 @@ class TodoContainer extends React.Component {
       todos: [...this.state.todos, newTodo],
     });
   };
-  setUpdate = (updatedTitle, id)=>{
+  setUpdate = (updatedTitle, id) => {
     this.setState({
-        todos: this.state.todos.map(todo => {
-            if(todo.id === id){
-                todo.tittle = updatedTitle
-            }
-            return todo
-        }),
-    })
-  }
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.tittle = updatedTitle;
+        }
+        return todo;
+      }),
+    });
+  };
 
   render() {
     return (
